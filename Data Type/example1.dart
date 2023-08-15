6178bb15953e2adcf809154a16f7811e64f76660
Abhishek Khinde <130373568+Abhishekkhinde@users.noreply.github.com>
2023-08-15 14:38:53 +0530

    Create example1.dart
    num, int , double   data type

---
+++ Data Type/example1.dart
@@ -0,0 +1,28 @@
+// num data type
+
+void main() {
+  int x = 10; // int data type
+  print(x);
+
+  num y = 20; // num data type
+  print(y);
+
+  num z = 30.5; // num  = num data type store int and float variable
+  print(z);
+
+  double a = 10.5; // double data type store floating  variable
+  print(a);
+
+  num b = 20.5;
+  print(b);
+
+  a = 30; // double can store intager value
+  print(a);
+
+  b = 40;
+  print(b);
+
+  print(a.runtimeType);  // runtimetype is  print the datatype of the variable
+  print(b.runtimeType);//
+  
+}
