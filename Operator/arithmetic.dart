6c2866bc6639d540e2a4dcbaa4d407a30abe1564
Abhishek Khinde <130373568+Abhishekkhinde@users.noreply.github.com>
2023-08-18 23:26:46 +0530

    arithmetic.dart

---
+++ Operator/arithmetic.dart
@@ -0,0 +1,15 @@
+// arithmetic opearator
+//  + , - , * , / , % , ~/
+
+void main() {
+  int x = 10;
+  int y = 8;
+
+  print(x + y);
+  print(x - y);
+  print(x / y);
+  print(x * y);
+  print(x % y);
+  print(x ~/ y);
+  
+}
