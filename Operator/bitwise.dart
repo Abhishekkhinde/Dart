4e4c4365f7d4dd80d500856098277fe8dd3908d6
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-08-21 12:36:33 +0530

    bitwise

---
+++ Operator/bitwise.dart
@@ -0,0 +1,16 @@
+// ~ , & , | , ^ , << , >>
+
+void main() {
+  int x = 14;
+  int y = 72;
+
+  print(x & y); // And &
+
+  print(x | y); // Or |
+
+  print(x ^ y); // Xor
+
+  print(x << 3); // left shift
+
+  print(y >> 4); // right shift
+}
