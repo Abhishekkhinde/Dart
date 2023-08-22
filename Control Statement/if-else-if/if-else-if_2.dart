3e5f0df3e4b1fdfc05e5e43f1229982dbd577eaa
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-08-22 21:48:29 +0530

    Add files via upload

---
+++ Control Statement/if-else-if/if-else-if_2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int marks = 80;
+
+  if (marks >= 95) {
+    print("IIT");
+  } else if (marks >= 85) {
+    print("MBBS");
+  } else if (marks >= 60) {
+    print("ENGINEERING");
+  } else {
+    print("BCS");
+  }
+
+  print("home");
+}
