3ac3f95a6cf362366fe30e2bbe5826bcd5140bef
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-08-22 21:48:11 +0530

    if_else-if

---
+++ Control Statement/if-else-if/if_else-if_1.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = -7;
+
+  if (number > 0) {
+    print("number us positive");
+  } else if (number < 0) {
+    print("number is negative"); // output
+  } else {
+    print("number is zero");
+  }
+}
