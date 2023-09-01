547c426680debc37f3539b143f529e44ed88d252
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-09-01 13:17:41 +0530

    while

---
+++ Control Statement/while loop/program_1.dart
@@ -0,0 +1,12 @@
+/*
+Program 1: Write a program to print a table of 2
+Output: 2 4 6 8 10 12 14 16 18 20 
+*/
+
+void main() {
+  int i = 1;
+  while (i <= 10) {
+    print(2 * i);
+    i++;
+  }
+}
