fbbaf9385be719d0b265e2fe6b344478326bb763
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-09-03 14:25:00 +0530

    Switch Case

---
+++ Control Statement/Switch Case/switch_2.dart
@@ -0,0 +1,15 @@
+void main() {
+  var x = "monday";
+  switch (x) {
+    case 1:
+    print("monday");
+    case 2:
+    print("sunday");
+    case 3:
+    print("three");
+    case 4:
+    print("four");
+    default:
+    print("No Match");
+  }
+}
---
+++ Control Statement/Switch Case/switch_3.dart
@@ -0,0 +1,15 @@
+void main() {
+  var x = "monday";
+  switch (x) {
+    case 1:
+    print("one");
+    case 2:
+    print("two");
+    case 1:
+    print("three");
+    case 1:
+    print("four");
+    default:
+    print("No Match");
+  }
+}
