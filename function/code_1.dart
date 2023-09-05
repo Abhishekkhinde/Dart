d2964a26dd69738d67bc1e5f244aa0c6260cd644
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-09-05 19:50:12 +0530

    function

---
+++ function/code_1.dart
@@ -0,0 +1,12 @@
+// function in dart
+void fun(String name, double sal) {
+  print("in fun");
+  print(name);
+  print(sal);
+}
+
+void main() {
+  print("start main");
+  fun('sandy', 50000);
+  print("end main");
+}
