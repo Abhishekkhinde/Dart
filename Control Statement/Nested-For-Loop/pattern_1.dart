cdf01c3889fd8a4fbb66f2f6912b9ffd4e39bbd2
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-09-09 19:53:42 +0530

    pattern

---
+++ Control Statement/Nested-For-Loop/pattern_1.dart
@@ -0,0 +1,15 @@
+/* C2W C2W C2W
+   C2W C2W C2W
+   C2W C2W C2W
+*/
+
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 3; i++) {
+    for (int j = 1; j <= 3; j++) {
+      stdout.write("C2W ");
+    }
+    print("");
+  }
+}
