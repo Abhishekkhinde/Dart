443bab9a6cd6c3431d2e02cb5de5df30e2d9c89b
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-09-12 17:35:37 +0530

    Create lambda_1.dart

---
+++ function/lambda function/lambda_1.dart
@@ -0,0 +1,10 @@
+// first way to declare anonymous function
+var add = (int a,  int b){
+  return a+b;
+};
+void main(){
+print((10,20));
+int x = 10;
+print(x.runtimeType);
+print(add.runtimeType);
+}
