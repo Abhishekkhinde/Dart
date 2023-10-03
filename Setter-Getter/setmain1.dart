6bf18265f86bc3b40abdd4e5909b6384e71a163a
Abhishek Khinde <abhisheknkhinde11@gail.com>
2023-10-03 17:27:05 +0530

    setter method 1

---
+++ Setter-Getter/setmain1.dart
@@ -0,0 +1,13 @@
+// setter 1st main method
+import 'setter1.dart';
+
+void main() {
+  Demo obj = new Demo(10, 'sam', 12.5);
+  obj.display();
+
+  obj.setx(15);
+  obj.setstr("sandy");
+  obj.setsal(75.45);
+
+  obj.display();
+}
