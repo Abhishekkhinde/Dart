69d91748cd9d5d8b4316c6b9543c05cdd0499e46
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-10-22 10:01:17 +0530

    constant constractor

---
+++ Constructor/constant_c4.dart
@@ -0,0 +1,14 @@
+class Demo {
+  final int? x;
+  final String? str;
+
+  const Demo(this.x, this.str);
+}
+
+void main() {
+  Demo dobj1 = const Demo(10, "kanha");
+  Demo dobj2 = const Demo(10, "vikas");
+
+  print(dobj1.hashCode);
+  print(dobj2.hashCode);
+}
