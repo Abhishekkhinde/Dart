cb991156e289e7982ffb7c472b722c3779c60416
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-11-01 02:39:10 +0530

    Hashset

---
+++ Collections/Set/HashSet_2.dart
@@ -0,0 +1,22 @@
+// Hash set
+
+import 'dart:collection';
+
+void main() {
+  Set jerno = {
+    232,
+    33,
+    24,
+    3,
+    433,
+    5,
+    434,
+    435,
+  };
+
+  print(jerno);
+  print(jerno.runtimeType);
+
+  var wings = HashSet();
+  print(wings.runtimeType);
+}
