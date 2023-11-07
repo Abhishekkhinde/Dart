e58067b7876332de8bae03c71f6b9c1cb08bff27
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2023-11-07 23:09:37 +0530

    iterable

---
+++ Collections/Iterator/Iterable Method/any.dart
@@ -0,0 +1,9 @@
+// any method
+
+void main() {
+  var players = ["Rohit", "Shubham", "Virat", "KLRahul", "Shreyash", "Hardik"];
+
+  var retval1 = players.any((player) => player[0] == "");
+
+  print(retval1);
+}
